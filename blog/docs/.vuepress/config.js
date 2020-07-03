180fa523a43e0c17daf448cd569714408049ca27
LeishenKOBE <wuyujiesong@outlook.com>
2020-07-04 01:04:17 +0800

    更新

---
+++ blog/docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: '小明同学的技术博客', 
+    description: '查阅知识',
+    themeConfig: {
+        search: true,//搜索
+        searchMaxSuggestions: 10,
+        nav:[
+            {text:"首页",link:"/"},
+            {text:"前端技术",link:"page1/"},
+            {text:"个人文章",items:[]}
+        ]
+    },
+    
+}
